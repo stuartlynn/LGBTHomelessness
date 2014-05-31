77dac7a8987bb3f51fcd94bc6f3c01206f87a4a4
Kelsey Ando <kma3a@virginia.edu>
2014-05-31 16:22:58 -0500

    added some scopes to the models

--- app/models/program.rb
+++ app/models/program.rb
@@ -3,5 +3,8 @@ class Program < ActiveRecord::Base
 
   scope :friendliness, ->(rate) {where(friendliness_rating: rate)}
   scope :age_range, -> (start_age,end_age){ where("min_age >= ?", start_age).where( "max_age <= ?", end_age)}
-
+  scope :gender_req, ->(gender){where(gender_req: gender)}
+  scope :health_status_req, ->(status){where(health_status_req: status)}
+  scope :language_req, ->(language){where(language_req: language)}
+  scope :services, ->(request){where("services like ?", request)}
 end
