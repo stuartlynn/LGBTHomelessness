87aac8752a8498cc48844d269a0382669e757dd1
Stuart <stuart@zooniverse.org>
2014-05-31 16:14:00 -0500

    some basic scopes

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,3 +1,7 @@
 class Program < ActiveRecord::Base
   belongs_to :organization
+
+  scope :friendliness, ->(rate) {where(friendliness_rating: rate)}
+  scope :age_range, -> (start_age,end_age){ where("min_age >= ?", start_age).where( "max_age <= ?", end_age)}
+
 end
