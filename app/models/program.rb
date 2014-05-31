a2cd465c1969844e4599d192384765d120fa4a32
Lauren Dorman <lauren@designisalso.com>
2014-05-31 18:27:13 -0500

    Front-end stuff

--- app/models/program.rb
+++ app/models/program.rb
@@ -3,5 +3,8 @@ class Program < ActiveRecord::Base
 
   scope :friendliness, ->(rate) {where(friendliness_rating: rate)}
   scope :age_range, -> (start_age,end_age){ where("min_age >= ?", start_age).where( "max_age <= ?", end_age)}
-
+  scope :gender_reqired, ->(gender){where(gender_req: gender)}
+  # scope :health_req, ->(status){where(health_status_req: status)}
+  scope :language_needed, ->(language){where(language_req: language)}
+  scope :services_offered, ->(request){where("services like ?", request)}
 end
