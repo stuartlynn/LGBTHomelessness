bb4c8ddf44da5cac12c3e284738be8eefe46566e
Lauren Dorman <lauren@designisalso.com>
2014-05-31 20:51:41 -0500

    Who form

--- config/initializers/secret_key.rb
+++ config/initializers/secret_key.rb
@@ -1,1 +1,1 @@
-LGBTHomelessness::Application.config.secret_token = ENV['SECRET_TOKEN']
+LGBTHomelessness::Application.config.secret_token = '893a4b7f0f3e80bb1caaf8a9b70d075da7816728dc614f04743804342462d1293f96c8ee2bc73ab2435496dfb82aa39cada902ff481f67a287abbeaa35a8e334'
