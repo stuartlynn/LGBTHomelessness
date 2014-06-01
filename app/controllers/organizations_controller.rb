03dbbf90182680a149dbe254f7dea05d372854c1
Stuart <stuart@zooniverse.org>
2014-06-01 00:15:12 -0500

    extra query params

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -4,7 +4,27 @@ class OrganizationsController < ActionController::Base
 
 
   def index
-    @organizations = Organization.all
+    query = Program
+    if(params[:age])
+      query = query.age_range(params[:age])
+    end
+
+    if(params[:sex])
+      query = query.gender_required(params[:sex])
+    end
+
+    if(params[:orientation])
+      query = query.sexual_orientation_required(params[:orientation])
+    end
+
+    if(params[:spanish])
+      query = query.language_needed(params[:spanish])
+    end
+
+
+    @organizations = query.all.collect{|p| p.organization}.uniq
+    puts "PROBLEM:" 
+    p @organizations
     respond_with @organizations
   end
 
