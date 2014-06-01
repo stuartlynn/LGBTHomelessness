f611a5fb59e7e7f1e67ffee65045beab2eeb7be0
Kelsey Ando <kma3a@virginia.edu>
2014-06-01 01:40:12 -0500

    adjusted filter on language

--- app/models/program.rb
+++ app/models/program.rb
@@ -6,7 +6,7 @@ class Program < ActiveRecord::Base
   scope :sexual_orientation_required, ->(orientation){where("sexual_orientation_requirement = ? OR sexual_orientation_requirement = '' ", orientation)}
   scope :gender_required, ->(gender){where("gender_req like ? OR gender_req = '' ", gender)}
   # scope :health_req, ->(status){where(health_status_req: status)}
-  scope :language_needed, ->(language){where(language_req: language)}
+  scope :language_needed, ->(language){where("language_req like ?", language)}
   scope :services_offered, ->(request){where("services like ?", request)}
 
   def self.survey_query(start_age, end_age, friendly = "", language = "", gender = "")
